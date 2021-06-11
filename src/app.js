341a15533f517f57d1c16848c3aeb390b90283cc
Steven Silvers <stevensilvers11@gmail.com>
2021-06-10 19:42:31 -0700

    Added !roll for a d100 and !d20 to roll a d20

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const tmi=require('tmi.js');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
-dotenv.config();
+dotenv.config({path:__dirname+'/../.env'});
 const client=new tmi.client({
   connection:{
     secure:true,
@@ -23,4 +23,19 @@ client.on('message',(channel,tags,message,self) =>{
   if(message.toLocaleLowerCase()==='!hello'){
     client.say(channel, `Welcome @${tags.username}!`)
   }
+  if(message.toLocaleLowerCase()==='!roll'){
+    var roll=RandomNumber(1,100);
+    client.say(channel, `@${tags.username} you rolled a ${roll}`);
+  }
+  if(message.toLocaleLowerCase()==='!d20'){
+    var roll=RandomNumber(1,20);
+    var msg=`@${tags.username} you rolled a ${roll} `.concat(roll===1 ? 'Critical Fail!' : roll===20 ? 'Critical Hit!' : '');
+    client.say(channel,msg);
+  }
 });
+
+function RandomNumber(min, max){
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
